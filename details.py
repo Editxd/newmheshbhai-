bb829561440fb7ec034d4b4fe8e98db74552f3b8
Editxd <152770568+Editxd@users.noreply.github.com>
2024-01-12 21:34:28 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
 api_id = 29410389
 api_hash = "0c716764715886f6641477ffbb63e1ee"
-bot_token = "5946934925:AAGoALZ3-Cr8hQaBdzHhmW3pknt-h1LVVpI"
-sudo_groups = -1001777454744
+bot_token = "6828258220:AAE5cfZIgP-_zg6dWoQHk6LQQ1zaf-Bn2Y4"
+sudo_groups = -1002028466553
 
